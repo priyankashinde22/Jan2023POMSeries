35f622cd44277e2360fb5ee3d006eed00ff6d52d
ramkrishna borhade <ramkrishnaborhade@ramkrishnas-mbp.lan>
2023-06-05 14:42:36 -0500

    feature3

--- src/main/java/com/qa/opencart/pages/NewPage.java
+++ src/main/java/com/qa/opencart/pages/NewPage.java
@@ -1,12 +1,13 @@
 package com.qa.opencart.pages;
 
 public class NewPage {
-<<<<<<< HEAD
-=======
+
 	
 	public void login() {
-		System.out.println("Anaika -login method");
+		System.out.println("Priyanka -login method");
+		int i =10,j=20;
+		System.out.println(i+j);
 	}
->>>>>>> c6a5f06ede30d08d35ba582db7acd62eaf8c9e9a
+
 
 }
