aa2ec9eeb194f2e96aaf49b1bed729f39df737e0
ramkrishna borhade <ramkrishnaborhade@ramkrishnas-mbp.lan>
2023-06-05 14:30:32 -0500

    Pop file added

---
+++ src/main/java/com/qa/opencart/pages/Pop.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class Pop {
+	
+	
+	public void login() {
+		System.out.println("Priyanka -login method");
+		int i =10,j=20;
+		System.out.println(i+j);
+	}
+
+}
