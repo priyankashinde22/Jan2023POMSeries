74e469947ab137c7b6cce4ebd84d75dc3aee3d1b
ramkrishna borhade <ramkrishnaborhade@ramkrishnas-mbp.lan>
2023-06-05 00:14:44 -0500

    env files updted

--- src/main/java/com/qa/opencart/pages/Feature1.java
+++ src/main/java/com/qa/opencart/pages/Feature1.java
@@ -3,8 +3,8 @@ package com.qa.opencart.pages;
 public class Feature1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Feature1");
 	}
 
 }
